3c2cb4d9e6d3b78e895f8a6d483af0c061de51d6
Heller, William T <hellerwt@ornl.gov>
2020-07-29 10:09:26 -0400

    Upload New File

---
+++ sas_temper/structurefactor.py
@@ -0,0 +1,32 @@
+r"""
+sas_temper
+
+structurefactor.py - a class that specifies a structure factor for a 
+                     model.  A YAML configuration file for sas_temper
+                     can also be used to specify that a model be multiplied
+                     by a structure factor.  In a sasmodels, a structure 
+                     factor is just another form of model for the purpose of
+                     the calculation.  Once calculated, sas_temper multiplies
+                     F(q)*S(q) for I(q) to compare against the data.  
+
+Oak Ridge National Laboratory, 2020
+
+"""
+
+import sa_fitter.param as param
+
+class StructureFactor(object):
+	def __init__(self, type, params):
+		self.params = []
+		
+		if type in ["harsphere", "hayter_msa", "squarewell", "stickyhardsphere"]:
+			self.type = type
+		else:
+			# we just set this rather then throw an error
+			self.type = "hardsphere"
+		
+		for a, par in enumerate(params):
+			self.params.append(par)
+			
+		
+	
