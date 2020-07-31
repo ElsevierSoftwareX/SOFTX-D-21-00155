b1507b03fbfe88c0f92c8b86451b8e0f00d42069
Heller, William T <hellerwt@ornl.gov>
2020-07-31 14:49:31 -0400

    Replace output.py

--- sas_temper/output.py
+++ sas_temper/output.py
@@ -212,7 +212,7 @@ def outputSetRes(conf, res):
                 if varkinds[j] not in ["fixed"]:
                     outbuf = "Plot pair " + str(i) + " and " + str(j)
                     print(outbuf)
-                    fig = plt.figure(figsize = [4,4], dpi=100)
+                    fig = plt.figure(figsize=(4,4))
                     print("created the fig")
                     grph = fig.add_subplot(1,1,1)
                     print("added the subplot")
