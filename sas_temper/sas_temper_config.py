cd7abd28337301fe9328f5e4ef0e8dafcd57b82a
Heller, William T <hellerwt@ornl.gov>
2020-07-31 12:43:20 -0400

    Replace sas_temper_config.py

--- sas_temper/sas_temper_config.py
+++ sas_temper/sas_temper_config.py
@@ -12,39 +12,39 @@ Oak Ridge National Laboratory, 2020
 import yaml
 
 def load_config(config_file="name"):
-	with open(config_file, 'r') as f:
-		return yaml.load(f)
-		
+    with open(config_file, 'r') as f:
+        return yaml.load(f)
+        
 
 class SAConfiguration(object):
-	def __init__(self, params):
-		self.datafile = params["name"]
-		self.qmin = params["qmin"]
-		self.qmax = params["qmax"]
-		self.output = params["output"]
-		self.temperatures = params["temperatures"]
-		self.temp_rate = params["temperature_rate"]
-		self.param_rate = params["parameter_rate"]
-		self.iterations = params["iterations"]
-		self.models = params["models"]
-		
-		#print ("Set up the SA configuration parameter class")
-		#print ("")
-		#print ("The file is " + str(self.datafile))
-		#outbuf = "    to be fit over q = " + str(self.qmin) + " to " + str(self.qmax)
+    def __init__(self, params):
+        self.datafile = params["name"]
+        self.qmin = params["qmin"]
+        self.qmax = params["qmax"]
+        self.output = params["output"]
+        self.temperatures = params["temperatures"]
+        self.temp_rate = params["temperature_rate"]
+        self.param_rate = params["parameter_rate"]
+        self.iterations = params["iterations"]
+        self.models = params["models"]
+        
+        #print ("Set up the SA configuration parameter class")
+        #print ("")
+        #print ("The file is " + str(self.datafile))
+        #outbuf = "    to be fit over q = " + str(self.qmin) + " to " + str(self.qmax)
         #print(outbuf)
-		#outbuf = "The output filename base is " + str(self.output)
+        #outbuf = "The output filename base is " + str(self.output)
         #print(outbuf)
-		#print ("")
-		#print ("The parameters for the simulated annealing:")
-		#outbuf = "     temperatures: " + str(self.temperatures)
+        #print ("")
+        #print ("The parameters for the simulated annealing:")
+        #outbuf = "     temperatures: " + str(self.temperatures)
         #print(outbuf)
-		#outbuf = "     temperature_rate: " + str(self.temp_rate)
+        #outbuf = "     temperature_rate: " + str(self.temp_rate)
         #print(outbuf)
-		#outbuf = "     parameter_rate: " + str(self.param_rate)
+        #outbuf = "     parameter_rate: " + str(self.param_rate)
         #print(outbuf)
-		#outbuf = "     iterations: " + str(self.iterations)
+        #outbuf = "     iterations: " + str(self.iterations)
         #print(outbuf)
-		#outbuf = "     models to generate: " + str(self.models)
+        #outbuf = "     models to generate: " + str(self.models)
         #print(outbuf)
-		
+        
