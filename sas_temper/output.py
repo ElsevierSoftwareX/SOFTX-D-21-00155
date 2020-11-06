12d7f6c76e4d685007588fba0ced7ec34e1ffc2c
Heller, William T <hellerwt@ornl.gov>
2020-11-06 07:12:04 -0500

    Replace output.py

--- sas_temper/output.py
+++ sas_temper/output.py
@@ -279,7 +279,7 @@ def outputFitCurve(conf, d, m, mnum, chisq):
     
     # set the plot title
     val = "%6.4f" %(chisq)
-    ax.set_title('Fit of profile '+str(mnum)+' to '+str(conf.datafile)+r' $\chi^2$='+val)
+    fig.set_title('Fit of profile '+str(mnum)+' to '+str(conf.datafile)+r' $\chi^2$='+val)
     
     # set the text of the axes
     ax.set_xlabel('q  (1/${\AA}$)')
