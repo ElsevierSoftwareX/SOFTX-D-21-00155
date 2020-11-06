1febacfb00c3ce8c2f8e9e6ab264efb450ad8c75
Heller, William T <hellerwt@ornl.gov>
2020-11-06 08:52:07 -0500

    Replace output.py

--- sas_temper/output.py
+++ sas_temper/output.py
@@ -287,8 +287,9 @@ def outputFitCurve(conf, d, m, mnum, chisq):
     
     # plot the data and the fit curve.
     ax.errorbar(d.x, d.y, yerr=d.dy, marker='o')
-    local_dy = np.zeros(len(m.x))
-    ax.errorbar(m.x, m.y, yerr=local_dy, color='r')
+    # local_dy = np.zeros(len(m.x))
+    # ax.errorbar(m.x, m.y, yerr=local_dy, color='r')
+    ax.plot(m.x,m.y,color='r')
     
     oname = str(conf.output)+"%02d.png" %(mnum)
     fig.savefig(oname,format='png')
