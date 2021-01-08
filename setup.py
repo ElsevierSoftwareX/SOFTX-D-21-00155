13ad1fbf3e9bfaf9b0e9bde68b080afb31962e87
Heller, William T <hellerwt@ornl.gov>
2021-01-08 16:44:13 -0500

    Replace setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def is_external_dependency(dependency):
 
 setup(
     name='sas_temper',
-    version='0.2.4',
+    version='0.3.0',
     description='SAS data analysis using simulated annealing and reproducibility characterization.  Uses sasmodels package',
     packages=['sas_temper'],
     scripts=['scripts/sas-temper'],
